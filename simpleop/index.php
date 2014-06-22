128eef7622e5726e02a55179b081556f07b1880f
Gaurav Gupta <gaurav.00g7@gmail.com>
2014-06-23 03:33:04 +0530

    Committing Edit2

--- simpleop/index.php
+++ simpleop/index.php
@@ -3,20 +3,23 @@
 require_once "dbconnect.php";
 include "functions.php";
 
-/* 
-	Including Functions.php file to test and run all the functionality that is 
-	Add an item, Delete an item, Edit and Update the item.
-*/
+$memcache = new Memcache;
+$memcache->connect('localhost', 11211) or die ("Could not connect");
+
 
 if(isset($_POST['addm'])){
 	$check = add_item($_POST['name'],$_POST['comments']);
 	if(!$check){
 		echo "<script>"."window.alert('Already in Database')"."</script>";
 	}
+	header('Location:index.php');
 }
 
 if(isset($_POST['update'])){
-	update_item($_POST['editname'],$_POST['editcomments'],$_GET['id']);
+	$checkedit = update_item($_POST['editname'],$_POST['editcomments'],$_GET['id'],$memcache);
+	if(!$checkedit){
+		echo "<script>"."window.alert('CANNOT PERFORM THIS ACTION')"."</script>";
+	}
 	header('Location:index.php');
 }
 
@@ -25,20 +28,16 @@ if(isset($_POST['cancel'])){
 }
 
 if(isset($_GET['delid'])){
-	delete_item($_GET['delid']);
+	delete_item($_GET['delid'],$memcache);
 	header('Location:index.php');
 }
 
 if(!isset($_GET['searchquery'])){
 $query = " SELECT * from `items` ";
-
-if(!$result = $db->query($query)){
-	die('There was an error running the query [' . $db->error . ']');
-}
 }
 else
 {
-	$result = search_item($_GET['searchquery']);
+	$query = search_item($_GET['searchquery'],$memcache);
 }
 
 
@@ -52,12 +51,9 @@ echo<<<END
 <head>
 	<meta property="og:image" content="http://www.belconference.in/images/bel/preview.jpg" />
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-	<link href="css/flexslider.css" rel="stylesheet" type="text/css" />
-	<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css" />
-	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all" />
 	<link href="css/bootstrap.icon-large.css" rel="stylesheet" type="text/css" />
 	<meta charset="utf-8">
-	<title>Wingify</title>
+	<title>Local-Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
@@ -81,19 +77,73 @@ echo<<<END
 				</thead> 
 				<tbody>
 END;
-				$num = 1;
-				while($row = $result->fetch_row()){
+				$i = 1;
 
+				$querynum = $query." ORDER BY `id` DESC LIMIT 1;";
+				$resultnum = $db->query($querynum);
+				$resultfetch= $resultnum->fetch_object();
+				$max = $resultfetch->id;
+
+				$result = $db->query($query);
+				if($result->num_rows==0){
+					echo "bye";
+					die;
+				}
+				$trace = $result->fetch_row();
+				
+				$num = $trace[0];
+
+				// echo "ROWS: ".$max."</br>";
+				// $memcache->delete("key1".$trace[0]);
+
+				while($num<=$max){
+					// echo "ID: ".$num." ";
+					$key1 = "key1".$num;
+					// echo "</br>".$key1."</br>";
+					if(!$memcache->get($key1) && !($memcache->get("del1".$num))){
+						// echo "SJNJNJCDC"."</br>";
+					$querytrace = $query." WHERE `id` = $num";
+					if(!$resultn = $db->query($querytrace)){
+						$num++;
+						continue;
+					}
+					if($resultn->num_rows==0){ $num++; continue;}
+					else{
+					$trace = $resultn->fetch_row(); 	
+					$memcache->set($key1,$trace, false , 500);
+					$row = $memcache->get($key1);
+					// echo $row[2]."</br>";
+				}
+					// $trace = $result->fetch_row();
+					}
+					elseif($memcache->get("del1".$num)){
+						echo "eneter";
+						$del1 = "del1".$num;
+						$memcache->delete($del1);
+						$memcache->delete($key1);
+						// echo "<script>"."window.alert('AREE')"."</script>";
+						$num++;
+						// $memcache->delete("del1".$num);
+						continue;
+					}
+					else{
+						$row = $memcache->get($key1);
+						// echo "Changed :".$row[2]."</br>";
+					}
+
+					// $memcache->delete($key1);
+					// $memcache->delete($key1);
+			
 
 				if(isset($_GET['id']) && $row[0] == $_GET['id']){
 				
-
+			
 echo<<<END
 
 				<tr>
-				<td width="5%">$num</td>
+				<td width="5%">$i</td>
 				<form action="index.php?id=$row[0]" method="POST">
-				<td style="width: 111px"><input style="width: 50%" type="text" name="editname" value="$row[1]"></td>
+				<td style="width: 111px"><input style="width: 80%" type="text" name="editname" value="$row[1]"></td>
 				<td><input style="width: 100%" type="message" name="editcomments" value="$row[2]" ></td>
 				<td><input type="submit" name="update" value="Update"></td>
 				<td><input type="submit" name="cancel" value="Cancel"></td>
@@ -101,13 +151,18 @@ echo<<<END
 				</form>
 				</tr>
 END;
-				$row = $result->fetch_row();
-				}
+				// $row = $result->fetch_row();
+				// $i++;
+				// $num++;
+				// $querytrace = $query." WHERE `id` = $num";
+				// $resultn = $db->query($querytrace);
+				// $row = $resultn->fetch_row();
+				}else{
 				
 				
 echo<<<END
 				<tr>
-				<td width="5%" id="num">$num</td>
+				<td width="5%" id="num">$i</td>
 				<td>$row[1]</td>
 				<td>$row[2]</td>
 				<td><button type="submit" name="edit" onClick="window.location.href = 'index.php?id=$row[0]'" value="$num"> Edit</button></td>
@@ -115,16 +170,18 @@ echo<<<END
 				</tr>
 				
 END;
+}
 				$num++;
+				$i++;
 			}
 
 			$result->free();
 			if(isset($_POST['add'])){
 echo<<<END
 				<tr>
-				<td width="5%">$num</td>
+				<td width="5%">$i</td>
 				<form action="index.php" method="POST">
-				<td style="width: 111px"><input id="name" style="width: 50%" type="text" name="name" ></td>
+				<td style="width: 111px"><input id="name" style="width: 80%" type="text" name="name" ></td>
 				<td><input id="comments" style="width: 100%" type="message" name="comments" ></td>
 				<td><input type="submit" name="addm" value="Submit" onClick="return test()"></td>
 				<td><input type="submit" name="cancel" value="Cancel"></td>
